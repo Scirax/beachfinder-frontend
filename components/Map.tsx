bb9433ee1b78e128e6763f66c72f26baa3c49f57
Athanasios Sampanis <athanas.sampanis@gmail.com>
2025-08-04 23:02:48 +0100

    fix: leaflet map center typing

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,17 +1,14 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 export default function Map() {
   return (
-    <MapContainer center={[38.2466, 21.7346]} zoom={7} style={{ height: '100vh', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[38.2466, 21.7346]}>
-        <Popup>
-          Example Beach
-        </Popup>
-      </Marker>
+    <MapContainer
+      center={[38.2466, 21.7346]}
+      zoom={7}
+      style={{ height: '100vh', width: '100%' }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
     </MapContainer>
   );
 }
